e8d7db2e6e0f1e4464f58a6ccefd9095073874d8
Marin Nikolli <sibalonat@gmail.com>
2024-12-30 18:43:14 +0100

    fix: this didnt work

--- src/Spreadsheet/DynamicSizeForCell.js
+++ src/Spreadsheet/DynamicSizeForCell.js
@@ -13,11 +13,9 @@ export function useDynamicResizeCell() {
     let activeRow = ref(null);
 
     // trck that it doesnt remove the entire row while dragging and resizing one cell
-    let initialY = ref(null);
 
     function handleMouseDown(event, element, list) {
         activeRow.value = list;
-        initialY.value = event.clientY;
         
         event.preventDefault();
         if (element) {
@@ -32,7 +30,6 @@ export function useDynamicResizeCell() {
     }
 
     function handleMouseMove(event) {
-        if (!resizingElement.value) return;
         const element = resizingElement.value;
 
         if (element) {
