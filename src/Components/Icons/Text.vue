640cef0f3532cbda757cd88720e6d67ccaec38ff
Marin Nikolli <sibalonat@gmail.com>
2025-01-17 21:42:20 +0100

    fix: create icons component for text, boolean, integer and datetime

---
+++ src/Components/Icons/Text.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+</script>
+<template>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 560 560" style="enable-background:new 0 0 560 560;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#EB6300;}
+	.st1{fill:#FDC100;}
+	.st2{fill:#FFC000;}
+</style>
+<path d="M498.51,551.57H61.1c-29.44,0-53.31-23.87-53.31-53.31V60.85c0-29.44,23.87-53.31,53.31-53.31h437.41
+	c29.44,0,53.31,23.87,53.31,53.31v437.41C551.82,527.7,527.95,551.57,498.51,551.57z"/>
+<g>
+	<polygon class="st1" points="134.99,99.19 134.99,166.14 173.92,166.14 173.92,73.6 	"/>
+	<polygon class="st1" points="122.98,107.09 87.34,130.51 87.34,166.14 122.98,166.14 	"/>
+	<polygon class="st1" points="43.75,166.14 75.32,166.14 75.32,138.41 43.75,158.24 	"/>
+</g>
+<rect x="43.75" y="180.25" class="st1" width="10.2" height="27.25"/>
+<rect x="67.74" y="180.25" class="st1" width="10.2" height="27.25"/>
+<rect x="91.74" y="180.25" class="st1" width="10.2" height="27.25"/>
+<rect x="115.73" y="180.25" class="st1" width="10.2" height="27.25"/>
+<rect x="139.72" y="180.25" class="st1" width="10.2" height="27.25"/>
+<rect x="163.72" y="180.25" class="st1" width="10.2" height="27.25"/>
+<rect x="43.75" y="221.61" class="st1" width="10.2" height="27.25"/>
+<rect x="188.2" y="305.76" class="st1" width="29.1" height="40.25"/>
+<rect x="188.2" y="387.19" class="st1" width="29.1" height="84.07"/>
+<rect x="230" y="305.76" class="st1" width="29.1" height="40.25"/>
+<rect x="67.74" y="221.61" class="st1" width="10.2" height="27.25"/>
+<rect x="91.74" y="221.61" class="st1" width="10.2" height="27.25"/>
+<rect x="257.25" y="217.44" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 501.4009 -50.2179)" class="st1" width="37.12" height="16.31"/>
+<rect x="257.25" y="305.05" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 589.0151 37.3964)" class="st1" width="37.12" height="16.31"/>
+<rect x="257.25" y="392.67" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 676.6294 125.0106)" class="st1" width="37.12" height="16.31"/>
+<rect x="257.25" y="480.28" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 764.2436 212.6248)" class="st1" width="37.12" height="16.31"/>
+<rect x="197.64" y="352.97" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 569.3403 163.8508)" class="st1" width="10.2" height="27.25"/>
+<rect x="192.62" y="483.24" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 699.6122 294.1226)" class="st0" width="20.25" height="27.25"/>
+<rect x="234.42" y="483.24" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 741.4131 252.3217)" class="st0" width="20.25" height="27.25"/>
+<rect x="239.44" y="352.97" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 611.1413 122.0498)" class="st1" width="10.2" height="27.25"/>
+<rect x="235.14" y="382.96" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 641.1323 152.0408)" class="st1" width="18.8" height="27.25"/>
+<rect x="219.66" y="432.74" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 690.9102 201.8186)" class="st1" width="49.78" height="27.25"/>
+<rect x="115.73" y="221.61" class="st1" width="10.2" height="27.25"/>
+<rect x="139.72" y="221.61" class="st1" width="10.2" height="27.25"/>
+<rect x="163.72" y="221.61" class="st1" width="10.2" height="27.25"/>
+<rect x="187.83" y="181.6" class="st1" width="29.84" height="110.64"/>
+<rect x="231.03" y="181.6" class="st1" width="27.04" height="110.64"/>
+<polygon class="st0" points="258.07,257.31 231.03,274.1 231.03,292.41 258.07,292.41 "/>
+<g>
+	<polygon class="st2" points="235.3,80.07 207.96,80.07 189.14,80.07 189.12,80.07 189.12,125.62 207.96,125.62 207.96,103.98 
+		235.3,103.98 	"/>
+	<polygon class="st0" points="235.3,103.98 235.3,80.06 206.28,80.06 	"/>
+	<path class="st1" d="M189.12,116.36v49.78h20.46c-4.87-3.58-8.03-9.35-8.03-15.86c0-10.87,8.81-19.68,19.68-19.68
+		s19.68,8.81,19.68,19.68c0,6.51-3.16,12.28-8.03,15.86h20.46v-49.78H189.12z"/>
+</g>
+<g>
+	<polygon class="st1" points="478.78,165.48 511.41,165.48 511.41,140.03 478.78,120.28 	"/>
+	<polygon class="st1" points="434.13,165.48 466.76,165.48 466.76,113.01 434.13,93.26 	"/>
+	<polygon class="st1" points="389.48,165.48 422.11,165.48 422.11,85.99 389.48,66.24 	"/>
+	<path class="st1" d="M350.21,165.48h27.25V58.97l-27.25-16.49V165.48z M354.72,68.41h16.31v17.41h-16.31V68.41z M354.72,109.5
+		h16.31v6.11h-16.31V109.5z M354.72,138.33h16.31v6.11h-16.31V138.33z"/>
+	<polygon class="st1" points="523.43,147.3 523.43,165.48 541.45,165.48 541.45,158.21 	"/>
+</g>
+</svg>
+
+</template>
