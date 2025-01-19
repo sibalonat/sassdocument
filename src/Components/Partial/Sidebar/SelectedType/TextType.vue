d9a377b5eb3ade6f2a6355e6eac2382b213d37f0
Marin Nikolli <sibalonat@gmail.com>
2025-01-19 19:44:07 +0100

    feat start with text size and preview

--- src/Components/Partial/Sidebar/SelectedType/TextType.vue
+++ src/Components/Partial/Sidebar/SelectedType/TextType.vue
@@ -1,9 +1,13 @@
 <script setup>
+import Options from '@/Components/Partial/Sidebar/SelectedType/TypeOptions/TxtOpts.vue'
 
 </script>
 
 <template>
-    <div>
+    <div class="relative w-full">
+        <Boolean class=" opacity-10" />
+        <Options />
+        <!-- <img src="" alt=""> -->
 
     </div>
 </template>
--- src/Components/Partial/Sidebar/SelectedType/TypeOptions/TxtOpts.vue
+++ src/Components/Partial/Sidebar/SelectedType/TypeOptions/TxtOpts.vue
@@ -2,8 +2,17 @@
 
 </script>
 <template>
-    <div>
-
+    <div class="p-4 bg-white rounded-lg shadow-md">
+        <div class="flex items">
+            <!-- <div class="w-1/2">
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <input type="text" name="name" id="name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+            </div>
+            <div class="w-1/2">
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <input type="text" name="name" id="name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+            </div> -->
+        </div>
     </div>
 </template>
 
