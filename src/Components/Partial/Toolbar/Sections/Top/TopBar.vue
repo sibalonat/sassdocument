f5510df4c1fc8f9f3dbd304237296457546ccc51
Marin Nikolli <sibalonat@gmail.com>
2025-01-22 23:38:01 +0100

    fix: add new font style icons (Bold, Italic, Color, StrikeThrough) to TopBar component

--- src/Components/Partial/Toolbar/Sections/Top/TopBar.vue
+++ src/Components/Partial/Toolbar/Sections/Top/TopBar.vue
@@ -6,6 +6,11 @@ import Increase from '@/Components/Icons/Spreadsheet/Integers/Decimal/Increase.v
 import Decrease from '@/Components/Icons/Spreadsheet/Integers/Decimal/Decrease.vue';
 import TextIncrease from '@/Components/Icons/Spreadsheet/General/Increase.vue';
 import TextDecrease from '@/Components/Icons/Spreadsheet/General/Decrease.vue';
+import Bold from '@/Components/Icons/Spreadsheet/General/FontStyle/Bold.vue';
+import Italic from '@/Components/Icons/Spreadsheet/General/FontStyle/Italic.vue';
+import Color from '@/Components/Icons/Spreadsheet/General/FontStyle/Color.vue';
+import StrikeThrough from '@/Components/Icons/Spreadsheet/General/FontStyle/StrikeThrough.vue';
+
 </script>
 <template>
     <div class="grid grid-cols-12 divide-x gap-x-1" >
@@ -16,10 +21,16 @@ import TextDecrease from '@/Components/Icons/Spreadsheet/General/Decrease.vue';
         <Increase />
         <More class="mb-auto" />
        </div> 
-       <div class="grid grid-cols-3 px-2 gap-x-2">
+       <div class="grid grid-cols-3 px-3 gap-x-2">
         <TextDecrease class="m-auto" />
         <input type="text" class="w-full h-8 text-center border rounded-md" />
         <TextIncrease class="m-auto" />
+       </div>
+       <div class="grid grid-cols-4 px-3 gap-x-2">
+        <Bold />
+        <Italic />
+        <StrikeThrough />
+        <Color />
        </div>
     </div>
 </template>
